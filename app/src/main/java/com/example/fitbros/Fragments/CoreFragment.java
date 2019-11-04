d759f6357d37f03e956e702a4651af14489ca6a0
Hasan Muslemani <w0721766@myscc.ca>
2019-11-03 19:44:10 -0500

    #5 Created Core Skeleton
    Changed layout to LinearLayout
    
    Made orientation vertical
    
    Added ListView with some muscles

--- app/src/main/java/com/example/fitbros/Fragments/CoreFragment.java
+++ app/src/main/java/com/example/fitbros/Fragments/CoreFragment.java
@@ -8,8 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.fitbros.NavType;
 import com.example.fitbros.R;
+
+import java.util.ArrayList;
 
 
 /**
@@ -17,6 +22,7 @@ import com.example.fitbros.R;
  */
 public class CoreFragment extends Fragment {
 
+    ListView listView;
 
     public CoreFragment() {
         // Required empty public constructor
@@ -26,8 +32,22 @@ public class CoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_core, container, false);
+        View view = inflater.inflate(R.layout.fragment_core, container, false);
+
+        listView = view.findViewById(R.id.coreListView);
+        final ArrayList<NavType> navTypes = new ArrayList<>();
+
+        navTypes.add(new NavType("Traverse abdominis", ""));
+        navTypes.add(new NavType("External abdominal oblique", ""));
+        navTypes.add(new NavType("Internal abdominal oblique ", ""));
+        navTypes.add(new NavType("Rectus abdominis", ""));
+
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, navTypes);
+
+        listView.setAdapter(adapter);
+
+        return view;
     }
 
 }
