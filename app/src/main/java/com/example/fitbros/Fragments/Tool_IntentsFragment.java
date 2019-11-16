b927e34ec5221286decadca449f191e02c46989e
Cordelle Neufeld <cordelle.neufeld01@stclairconnect.ca>
2019-11-16 13:38:12 -0500

    #29 Tool intent for map of local gyms * Created tool intents fragment with x4 buttons for common intents * Externalized strings

---
+++ app/src/main/java/com/example/fitbros/Fragments/Tool_IntentsFragment.java
@@ -0,0 +1,87 @@
+package com.example.fitbros.Fragments;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.fitbros.R;
+
+public class Tool_IntentsFragment extends Fragment {
+
+    Button alarmButton;
+    Button twitterButton;
+    Button mapButton;
+    Button webButton;
+
+    public Tool_IntentsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_tool_intents, container, false);
+
+        alarmButton = view.findViewById(R.id.buttonGymAlarm);
+        twitterButton = view.findViewById(R.id.buttonGymTwitter);
+        mapButton = view.findViewById(R.id.buttonGymFinder);
+        webButton = view.findViewById(R.id.buttonGymFitTips);
+
+        alarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO
+
+            }
+        });
+
+        twitterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO
+
+            }
+        });
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri geoLocation = Uri.parse("geo:0.0?q=fitness%20or%20gym");
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+
+                intent.setData(geoLocation);
+
+                // ensure that there is map functionality
+                if(intent.resolveActivity(getActivity().getPackageManager()) !=null) {
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        webButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO
+
+            }
+        });
+
+        return view;
+    }
+
+}
